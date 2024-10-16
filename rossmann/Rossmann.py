af88f3f3dac60548093fab785406a35adf1b7629
Jessica Nadalete <99223015+jessicanadalete@users.noreply.github.com>
2024-10-16 06:30:57 -0300

    Update Rossmann.py

--- rossmann/Rossmann.py
+++ rossmann/Rossmann.py
@@ -34,15 +34,12 @@ class Rossmann( object ):
     df1['date'] = pd.to_datetime(df1['date'])
 
     ###2.5 Fillout NA
-    df1['competition_distance'].max()
-    #competition distance = distance in meters to the nearest competidor store
+  
     #-> considering that NA is when the store doesnt have any competidor near, we'll input a bigger distance then max distance in the data
-    # here Im using only one column
     df1['competition_distance'] = df1['competition_distance'].apply(lambda x: 200000.0 if math.isnan(x) else x)
 
     #competition_open_since_month and year = year and month of the time the nearest competidor was opened
     #->assumption: considering the same date of the sale
-    #obs: here Im using more than one column of my dataframe
     df1['competition_open_since_month'] = df1.apply(lambda x: x['date'].month if math.isnan(x['competition_open_since_month']) else x['competition_open_since_month'], axis=1)
     df1['competition_open_since_year'] = df1.apply(lambda x: x['date'].year if math.isnan(x['competition_open_since_year']) else x['competition_open_since_year'], axis=1)
 
@@ -119,7 +116,7 @@ class Rossmann( object ):
     return df2
 
   def data_preparation( self, df5 ):
-
+    ###5.2 Rescaling
     #competition_distante
     df5['competition_distance'] = self.competition_distance_scaler.fit_transform( df5[['competition_distance']].values )
 
@@ -142,9 +139,6 @@ class Rossmann( object ):
     # assortment - Ordinal Encoding
     assortment_dict = {'basic': 1, 'extra': 2, 'extended': 3}
     df5['assortment'] = df5['assortment'].map( assortment_dict )
-
-    ###5.2.2 Response Variable Transformation
-    df5['sales'] = np.log1p( df5['sales'] )
 
     ###5.2.3 Nature Transformation
 
